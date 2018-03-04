78d10ec11a5a30c247c6ac71f00770766bce735b
Unknown <inphoffmann@gmail.com>
2018-03-04 14:54:02 -0300

    He encontrado la función QFileDialog que me permite elegir un archivo.

--- Herramental2.py
+++ Herramental2.py
@@ -1,6 +1,7 @@
-import sys
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
+from PyQt5.QtWidgets import (QMainWindow, QTextEdit, 
+    QAction, QFileDialog, QApplication)
 from PyQt5.QtGui import QIcon
+import sys
 
 class Example(QMainWindow):
     
@@ -10,19 +11,36 @@ class Example(QMainWindow):
         self.initUI()
         
         
-    def initUI(self):               
+    def initUI(self):      
+
+        self.textEdit = QTextEdit()
+        self.setCentralWidget(self.textEdit)
+        self.statusBar()
+
+        openFile = QAction(QIcon('open.png'), 'Open', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open new File')
+        openFile.triggered.connect(self.showDialog)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openFile)       
         
-        exitAct = QAction(QIcon('exit24.png'), 'Exit', self)
-        exitAct.setShortcut('Ctrl+Q')
-        exitAct.triggered.connect(self.close)
-        
-        self.toolbar = self.addToolBar('Exit')
-        self.toolbar.addAction(exitAct)
-        
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('Toolbar')    
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('File dialog')
         self.show()
         
+        
+    def showDialog(self):
+
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')
+
+        if fname[0]:
+            f = open(fname[0], 'r')
+
+            with f:
+                data = f.read()
+                self.textEdit.setText(data)        
         
 if __name__ == '__main__':
     
